5895dadeac6fbfd00bf2b2f5df72b0e3b915077d
kobanagc <naoki.kobayashi.0708@gmail.com>
2021-12-16 22:53:22 +0900

    add Reaction button

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
   resources :users
-  post "users/likes/:id/create" => "likes#create"
+  post "users/reactions/:id" => "reactions#create"
   post "users/likes/:id/destroy" => "likes#destroy"
   resources :rooms do
     resources :messages
--- db/migrate/20210304144827_create_reactions.rb
+++ db/migrate/20210304144827_create_reactions.rb
@@ -1,8 +1,8 @@
 class CreateReactions < ActiveRecord::Migration[6.0]
   def change
     create_table :reactions do |t|
-      t.integer :to_user_id
-      t.integer :from_user_id
+      t.integer :to_user_id, foreign_key: true
+      t.integer :from_user_id, foreign_key: true
       t.integer :status
       t.timestamps
     end
